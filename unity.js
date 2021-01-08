99f12402c07402128368e2c03d17be6cbba2e2df
ikaros <609850206@qq.com>
2021-01-08 11:13:14 +0800

    unity1.0带注释

--- unity.js
+++ unity.js
@@ -1,8 +1,10 @@
-function $(element){
+//$()获取原生元素对象
+function $(element) {
     return document.querySelector(element);
 }
 
-function readimg(imgFile,img){
+//在当前页面显示上传的img图片
+function readimg(imgFile, img) {
     $(imgFile).onchange = function () {
         if (this.files.length) {
             let file = this.files[0];
@@ -15,19 +17,22 @@ function readimg(imgFile,img){
     }
 }
 
-Number.prototype.addZero = function(l){
+//返回x位数字，以0补位
+Number.prototype.addZero = function (x) {
     let s = '';
-    for(let i = this.toString().length; i < l; i++){
+    for (let i = this.toString().length; i < x; i++) {
         s += '0'
     }
     s += this;
     return s
 }
 
-Element.prototype.rotation = function (imgArr,t) {
-    let r = this;
-    let timer;
-    let rWidth = r.clientWidth;
+//自动生产轮播图
+Element.prototype.rotation = function (imgArr, t) {
+    let r = this; //轮播图容器
+    let timer; //定时器
+    let rWidth = r.clientWidth; //容器宽度
+    //拼接字符串
     let HTMLStr = ` <div style="transform: translateX(${-rWidth}px);">
         <div style="width:${rWidth}px">
             <img src="${imgArr[imgArr.length-1]}">
@@ -63,34 +68,42 @@ Element.prototype.rotation = function (imgArr,t) {
                     <p>R</p>
                 </div> `
     r.innerHTML = HTMLStr;
-    let index = 0;
-    let pdz = 0 - r.children[0].clientWidth + rWidth;
-    let r_left = -rWidth;
-    autoplay(t)
-    r.children[2].onclick = left;
-    r.children[3].onclick = right;
 
+    let index = 0; //图片索引
+    let pdz = 0 - r.children[0].clientWidth + rWidth; //判断值
+    let r_left = -rWidth; //当前向左移动的值
+    autoplay(t) // 执行轮播函数
+    r.children[2].onclick = left; //左按钮绑定
+    r.children[3].onclick = right; //右按钮绑定
+    //鼠标移入清除定时器
     r.onmouseover = () => {
         clearInterval(timer)
     };
+    //鼠标移出打开定时器
     r.onmouseout = () => {
         autoplay(5000)
     };
-    r.children[1].children[0].onclick = liC;
-    r.children[1].children[1].onclick = liC;
-    r.children[1].children[2].onclick = liC;
+    //li元素绑定点击事件
+    for (let i = 0; i < imgArr.length; i++) {
+        r.children[1].children[i].onclick = liC;
+    }
 
+    //li元素点击事件
     function liC(e) {
-        let index = Number(e.target.attributes[0].value);
+        let index = Number(e.target.attributes[0].value);//点击的li元素index索引值
+        //清空active类名
         for (let i = 0; i < 3; i++) {
             r.children[1].children[i].className = '';
         }
+        //赋予类名
         r.children[1].children[index].className = 'active';
+        //移动的值
         r_left = (index + 1) * -rWidth;
         r.children[0].style.transition = `transform .3s`;
         r.children[0].style.transform = `translateX(${r_left}px)`;
     }
 
+    //轮播函数
     function autoplay(t) {
         timer = setInterval(() => {
             if (r_left <= pdz) {
@@ -123,6 +136,7 @@ Element.prototype.rotation = function (imgArr,t) {
         }, t);
     }
 
+    //左按钮函数
     function left() {
         for (let i = 0; i < 3; i++) {
             r.children[1].children[i].className = '';
@@ -146,6 +160,7 @@ Element.prototype.rotation = function (imgArr,t) {
         r.children[1].children[index].className = 'active';
     }
 
+    //右按钮函数
     function right() {
         for (let i = 0; i < 3; i++) {
             r.children[1].children[i].className = '';
