78a604481a74b3ce313776dfd8ab010cb4153a1b
ikaros <609850206@qq.com>
2021-01-14 00:17:55 +0800

    unity1.1

--- unity.js
+++ unity.js
@@ -1,10 +1,8 @@
-//$()获取原生元素对象
-function $(element) {
+function $(element){
     return document.querySelector(element);
 }
 
-//在当前页面显示上传的img图片
-function readimg(imgFile, img) {
+function readimg(imgFile,img){
     $(imgFile).onchange = function () {
         if (this.files.length) {
             let file = this.files[0];
@@ -17,87 +15,82 @@ function readimg(imgFile, img) {
     }
 }
 
-//返回x位数字，以0补位
-Number.prototype.addZero = function (x) {
+Number.prototype.addZero = function(l){
     let s = '';
-    for (let i = this.toString().length; i < x; i++) {
+    for(let i = this.toString().length; i < l; i++){
         s += '0'
     }
     s += this;
     return s
 }
 
-//自动生产轮播图
-Element.prototype.rotation = function (imgArr, t) {
-    let r = this; //轮播图容器
-    let timer; //定时器
-    let rWidth = r.clientWidth; //容器宽度
-    //拼接字符串
+Element.prototype.rotation = function (imgArr,t) {
+    let r = this;
+    let timer;
+    let rWidth = r.clientWidth;
     let HTMLStr = ` <div style="transform: translateX(${-rWidth}px);">
-                        <div style="width:${rWidth}px">
-                            <img src="${imgArr[imgArr.length-1]}">
-                        </div>
+        <div style="width:${rWidth}px">
+            <img src="${imgArr[imgArr.length-1]}">
+        </div>
         `
     for (let i = 0; i < imgArr.length; i++) {
         HTMLStr += `
         <div style="width:${rWidth}px">
-            <img src="${imgArr[i]}">
+        <img src="${imgArr[i]}">
         </div>`
     }
-    HTMLStr += `    <div style="width:${rWidth}px">
-                        <img src="${imgArr[0]}">
-                    </div>
-                </div>
-                <ul>
+    HTMLStr += `<div style="width:${rWidth}px">
+        <img src="${imgArr[0]}">
+        </div>
+        </div>
+        <ul>
         `
     for (let i = 0; i < imgArr.length; i++) {
         if (i == 0) {
-            HTMLStr += `<li index='${i}' class="active"></li>`
+            HTMLStr += `
+        <li index='${i}' class="active"></li>`
         } else {
-            HTMLStr += `<li index='${i}'></li>`
+            HTMLStr += `
+        <li index='${i}'></li>`
         }
 
     }
     HTMLStr += `</ul>
                 <div class="left">
+                    <div></div>
                 </div>
                 <div class="right">
+                    <div></div>
                 </div> `
     r.innerHTML = HTMLStr;
+    let index = 0;
+    let pdz = 0 - r.children[0].clientWidth + rWidth;
+    let r_left = -rWidth;
+    autoplay(t)
+    r.children[2].onclick = left;
+    r.children[3].onclick = right;
 
-    let index = 0; //图片索引
-    let pdz = 0 - r.children[0].clientWidth + rWidth; //判断值
-    let r_left = -rWidth; //当前向左移动的值
-    autoplay(t) // 执行轮播函数
-    r.children[2].onclick = left; //左按钮绑定
-    r.children[3].onclick = right; //右按钮绑定
-    //鼠标移入清除定时器
     r.onmouseover = () => {
         clearInterval(timer)
     };
-    //鼠标移出打开定时器
     r.onmouseout = () => {
-        autoplay(t)
+        autoplay(5000)
     };
-    //li元素绑定点击事件
-    for (let i = 0; i < imgArr.length; i++) {
-        r.children[1].children[i].onclick = liC;
-    }
+    r.children[1].children[0].onclick = liC;
+    r.children[1].children[1].onclick = liC;
+    r.children[1].children[2].onclick = liC;
 
-    //li元素点击事件
     function liC(e) {
-        let index = Number(e.target.attributes[0].value);//点击的li元素index索引值
-        //清空active类名
-        clearClass();
-        //赋予类名
+        let index = Number(e.target.attributes[0].value);
+        for (let i = 0; i < 3; i++) {
+            r.children[1].children[i].className = '';
+        }
         r.children[1].children[index].className = 'active';
-        //移动的值
         r_left = (index + 1) * -rWidth;
         r.children[0].style.transition = `transform .3s`;
         r.children[0].style.transform = `translateX(${r_left}px)`;
     }
 
-    //轮播函数
     function autoplay(t) {
         timer = setInterval(() => {
             if (r_left <= pdz) {
@@ -105,8 +98,9 @@ Element.prototype.rotation = function (imgArr, t) {
                 r.children[0].style.transform = `translateX(${-rWidth}px)`;
                 r_left = -rWidth;
                 index++;
-                //清空active类名
-                clearClass();
+                for (let i = 0; i < 3; i++) {
+                    r.children[1].children[i].className = '';
+                }
                 r.children[1].children[index].className = 'active';
                 setTimeout(() => {
                     r_left -= rWidth;
@@ -118,8 +112,9 @@ Element.prototype.rotation = function (imgArr, t) {
                 r.children[0].style.transition = `transform .3s`;
                 r.children[0].style.transform = `translateX(${r_left}px)`;
                 index++;
-                //清空active类名
-                clearClass();
+                for (let i = 0; i < 3; i++) {
+                    r.children[1].children[i].className = '';
+                }
                 if (index == 3) {
                     index = 0;
                 }
@@ -128,10 +123,10 @@ Element.prototype.rotation = function (imgArr, t) {
         }, t);
     }
 
-    //左按钮函数
     function left() {
-        //清空active类名
-        clearClass();
+        for (let i = 0; i < 3; i++) {
+            r.children[1].children[i].className = '';
+        }
         if (index == 0) {
             index = 2;
             r_left = 0;
@@ -151,10 +146,10 @@ Element.prototype.rotation = function (imgArr, t) {
         r.children[1].children[index].className = 'active';
     }
 
-    //右按钮函数
     function right() {
-        //清空active类名
-        clearClass();
+        for (let i = 0; i < 3; i++) {
+            r.children[1].children[i].className = '';
+        }
         if (index == 2) {
             index = 0;
             r_left = pdz;
@@ -172,12 +167,5 @@ Element.prototype.rotation = function (imgArr, t) {
             r.children[0].style.transform = `translateX(${r_left}px)`;
         }
         r.children[1].children[index].className = 'active';
-    }
-    
-    //清空active类名
-    function clearClass(){
-        for (let i = 0; i < imgArr.length; i++) {
-            r.children[1].children[i].className = '';
-        }
     }
 }
